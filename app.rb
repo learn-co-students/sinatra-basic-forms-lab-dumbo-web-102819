ab67cc533e48fbdcc840dc6981a8b8461b06e61d
Dominic Chu <dominic.m.chu@gmail.com>
2019-11-20 10:09:00 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,5 +1,23 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/' do 
+        # has a link with the text "List A Puppy"
+        erb :index
+        # has a link to list a puppy that links to /new
+    end 
 
+    get '/new' do 
+        # renders a form that can POST a name, breed, and age
+        erb :create_puppy
+    end 
+
+    post '/puppy' do 
+        @name = params[:name]
+        @breed = params[:breed]
+        @age = params[:age]
+        #recieves a req from /new at /puppy
+        #displays the puppy info dynamically
+        erb :display_puppy
+    end 
 end
